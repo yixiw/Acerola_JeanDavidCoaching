5eb68aaeb4ec1a9e6eec4ddf40cde6bb1c66a539
21702654 <21702654@a17714.ad.ut-capitole.fr>
2019-03-27 18:40:13 +0100

    commit consulter seance

--- AcerolaJeanDavidCoaching/src/java/bd/BD.java
+++ AcerolaJeanDavidCoaching/src/java/bd/BD.java
@@ -5,10 +5,32 @@
  */
 package bd;
 
+import java.util.List;
+import metier.Seancestandard;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author yuwei
  */
 public class BD {
-    ///FEF
+     /*----- Ouverture de la session et de la transaction -----*/
+    static Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+    Transaction t = session.beginTransaction();
+    public BD() {
+    }
+    /**
+        * Retour de la requête sous forme d'une liste de tableau d'objets SeanceStandard.
+     * @param bilan
+     * @return 
+    */
+    public  static List<Seancestandard> consulterListeSeance (int bilan)  {
+       
+        List listeSeance;
+        listeSeance = (List) session.createQuery("FROM Seancestandard s where bilanss ="+bilan).list();
+        
+        return listeSeance;
+    
+    }
 }
